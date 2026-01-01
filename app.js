45797e251200c3d31b7d03b07527c2de52e416f2
IbrohimIgaybullayev <igaybullayev10@gmail.com>
2026-01-01 19:51:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,293 @@
+const myQuestions = [
+    {
+        id: 1,
+        question: "I have ......... to London four times so far.",
+        options: { a: "gone", b: "been", c: "gone to", d: "commuted" },
+        correct: "b"
+    },
+    {
+        id: 2,
+        question: "Find the correct sentence.",
+        options: { a: "He shall go the exhibiton center", b: "I shall visit that country next month", c: "We shall go shopping in next week", d: "Shall you have a meeting tomorrow?" },
+        correct: "b"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 4,
+        question: "You are not taking part in today's competition, .......",
+        options: { a: "are you?", b: "are not you?", c: "aren't you?", d: "will you?" },
+        correct: "c"
+    },
+    {
+        id: 5,
+        question: "I am one of the participants in this tournament, .......",
+        options: { a: "am not I?", b: "amn't I?", c: "isn't I?", d: "aren't I?" },
+        correct: "d"
+    },
+    {
+        id: 6,
+        question: "The news in this channel ...... never been misleading, ........",
+        options: { a: "have/is they?", b: "have/aren't they?", c: "has/isn't it?", d: "has/is it?" },
+        correct: "d"
+    },
+    {
+        id: 7,
+        question: "None these souvenirs are affordable, ........",
+        options: { a: "are they?", b: "aren't they?", c: "are them?", d: "aren't them?" },
+        correct: "a"
+    },
+    {
+        id: 8,
+        question: "Tom purchased ........ piano yesterday.",
+        options: { a: "the", b: "a", c: "—", d: "an" },
+        correct: "b"
+    },
+    {
+        id: 9,
+        question: "He has been able to ........ for 2 years, ........",
+        options: { a: "play the guitar/hasn't he?", b: "sing the guitar/hasn't he?", c: "play a guitar/hasn't he?", d: "play a guitar/has he?" },
+        correct: "a"
+    },
+    {
+        id: 10,
+        question: "The plane to Dubai ....... at 5 am tomorrow. ",
+        options: { a: "will leave", b: "leaves", c: "is leaving", d: "will leaves" },
+        correct: "b"
+    },
+    {
+        id: 11,
+        question: "If I ....... you, I would just embrace that offer.",
+        options: { a: "were", b: "was", c: "am", d: "are" },
+        correct: "a"
+    },
+    {
+        id: 12,
+        question: "I wish I ...... able to fly.",
+        options: { a: "am", b: "will", c: "will be", d: "was" },
+        correct: "d"
+    },
+    {
+        id: 13,
+        question: "If only he ........ the chance to travel abroad.  ",
+        options: { a: "has", b: "had", c: "was", d: "owns" },
+        correct: "b"
+    },
+    {
+        id: 14,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 15,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 16,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 17,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 18,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 19,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 20,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 21,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 22,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 23,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 24,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 25,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 26,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 27,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 28,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 29,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 30,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+    {
+        id: 3,
+        question: "That was the house ......... last month.",
+        options: { a: "whose I wanted to buy", b: ", which I wanted to buy", c: "which I wanted to buy", d: "which I will wanted to buy" },
+        correct: "c"
+    },
+
+];
+
+function displayQuiz() {
+    const wrapper = document.getElementById('quiz-wrapper');
+    if (!wrapper) return; // Agar HTML-da element topilmasa xato bermasligi uchun
+
+    let htmlContent = '';
+
+    myQuestions.forEach((q) => {
+        // DIQQAT: Pastdagi belgi oddiy qo'shtirnoq emas, BACKTICK (  )
+        htmlContent += `
+                  <div class="question-block" id="block-${q.id}">
+                <span class="question-text">${q.id}. ${q.question}</span>
+                <ul class="options">
+                    <li id="li-${q.id}-a"><input type="radio" name="q${q.id}" value="a"> A) ${q.options.a}</li>
+                    <li id="li-${q.id}-b"><input type="radio" name="q${q.id}" value="b"> B) ${q.options.b}</li>
+                    <li id="li-${q.id}-c"><input type="radio" name="q${q.id}" value="c"> C) ${q.options.c}</li>
+                    <li id="li-${q.id}-d"><input type="radio" name="q${q.id}" value="d"> D) ${q.options.d}</li>
+                </ul>
+            </div>
+       
+        `;
+    });
+    wrapper.innerHTML = htmlContent;
+}
+
+function calculateScore() {
+    let score = 0;
+    myQuestions.forEach((q) => {
+        const selected = document.querySelector(`input[name="q${q.id}"]:checked`);
+        document.querySelectorAll(`#block-${q.id} li`).forEach(li => li.classList.remove('correct', 'wrong'));
+        document.getElementById(`li-${q.id}-${q.correct}`).classList.add('correct');
+
+        if (selected) {
+            if (selected.value === q.correct) {
+                score++;
+            } else {
+                document.getElementById(`li-${q.id}-${selected.value}`).classList.add('wrong');
+            }
+        }
+    });
+    document.getElementById('result-area').innerHTML = `Your Score: ${score} / ${myQuestions.length}`;
+}
+
+window.onload = displayQuiz;
